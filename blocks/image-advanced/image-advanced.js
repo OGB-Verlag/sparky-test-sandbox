cc5f16a0737f125d9523767f1ba6382cc0144a73
Teodor Iancu <teodor.iancu@oegbverlag.at>
2024-07-20 23:18:56 +0200

    dfg

--- blocks/image-advanced/image-advanced.js
+++ blocks/image-advanced/image-advanced.js
@@ -1,3 +1,19 @@
 export default function decorate(block) {
-  console.log('block::', block)
+  const firstDiv = block.querySelector('div')
+
+  if (firstDiv) {
+    switch (true) {
+      case block.classList.contains('image-advanced-align-right'):
+        firstDiv.style.textAlign = 'right'
+        break
+      case block.classList.contains('image-advanced-align-left'):
+        firstDiv.style.textAlign = 'left'
+        break
+      case block.classList.contains('image-advanced-align-center'):
+        firstDiv.style.textAlign = 'center'
+        break
+      default:
+        break
+    }
+  }
 }
