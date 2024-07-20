9dd502ddd56950a065388cdd3df1798acd7c686b
Teodor Iancu <teodor.iancu@oegbverlag.at>
2024-07-20 23:24:59 +0200

    fg

--- blocks/image-advanced/image-advanced.js
+++ blocks/image-advanced/image-advanced.js
@@ -2,18 +2,12 @@ export default function decorate(block) {
   const firstDiv = block.querySelector('div')
 
   if (firstDiv) {
-    switch (true) {
-      case block.classList.contains('image-advanced-align-right'):
-        firstDiv.style.textAlign = 'right'
-        break
-      case block.classList.contains('image-advanced-align-left'):
-        firstDiv.style.textAlign = 'left'
-        break
-      case block.classList.contains('image-advanced-align-center'):
-        firstDiv.style.textAlign = 'center'
-        break
-      default:
-        break
+    if (block.classList.contains('image-advanced-align-right')) {
+      firstDiv.classList.add('image-advanced-align-right')
+    } else if (block.classList.contains('image-advanced-align-left')) {
+      firstDiv.classList.add('image-advanced-align-left')
+    } else if (block.classList.contains('image-advanced-align-center')) {
+      firstDiv.classList.add('image-advanced-align-center')
     }
   }
 }
