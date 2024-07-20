b7ce2f59fe37115e2e9ebac06c2a49d639ee8f25
Teodor Iancu <teodor.iancu@oegbverlag.at>
2024-07-20 14:46:27 +0200

    rev

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,7 +2,7 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children]
   block.classList.add(`columns-${cols.length}-cols`)
 
-  // setup image and video columns
+  // setup image columns
   ;[...block.children].forEach((row) => {
     ;[...row.children].forEach((col) => {
       const pic = col.querySelector('picture')
@@ -11,15 +11,6 @@ export default function decorate(block) {
         if (picWrapper && picWrapper.children.length === 1) {
           // picture is only content in column
           picWrapper.classList.add('columns-img-col')
-        }
-      }
-
-      const video = col.querySelector('video')
-      if (video) {
-        const videoWrapper = video.closest('div')
-        if (videoWrapper && videoWrapper.children.length === 1) {
-          // video is only content in column
-          videoWrapper.classList.add('columns-video-col')
         }
       }
     })
