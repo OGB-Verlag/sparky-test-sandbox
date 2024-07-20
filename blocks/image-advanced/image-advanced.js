145e2bffc3091a746a2f4e50102e72e2cd3e2492
Teodor Iancu <teodor.iancu@oegbverlag.at>
2024-07-20 23:37:50 +0200

    sdfg

--- blocks/image-advanced/image-advanced.js
+++ blocks/image-advanced/image-advanced.js
@@ -1,12 +1,16 @@
 export default function decorate(block) {
   const wrapperDiv = block.closest('.image-advanced-wrapper')
+
   if (wrapperDiv) {
     if (block.classList.contains('image-advanced-align-right')) {
       wrapperDiv.classList.add('image-advanced-align-right')
+      block.classList.remove('image-advanced-align-right')
     } else if (block.classList.contains('image-advanced-align-left')) {
       wrapperDiv.classList.add('image-advanced-align-left')
+      block.classList.remove('image-advanced-align-left')
     } else if (block.classList.contains('image-advanced-align-center')) {
       wrapperDiv.classList.add('image-advanced-align-center')
+      block.classList.remove('image-advanced-align-center')
     }
   }
 }
