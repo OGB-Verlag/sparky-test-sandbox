95dda7bf13b8b3af683f6b580fa4c0921d26acc0
Teodor Iancu <teodor.iancu@oegbverlag.at>
2024-07-21 12:27:35 +0200

    image-advanced size

--- blocks/image-advanced/image-advanced.js
+++ blocks/image-advanced/image-advanced.js
@@ -1,16 +1,24 @@
 export default function decorate(block) {
   const wrapperDiv = block.closest('.image-advanced-wrapper')
-
   if (wrapperDiv) {
-    if (block.classList.contains('image-advanced-align-right')) {
-      wrapperDiv.classList.add('image-advanced-align-right')
-      block.classList.remove('image-advanced-align-right')
-    } else if (block.classList.contains('image-advanced-align-left')) {
-      wrapperDiv.classList.add('image-advanced-align-left')
+    if (block.classList.contains('image-advanced-align-left')) {
       block.classList.remove('image-advanced-align-left')
-    } else if (block.classList.contains('image-advanced-align-center')) {
-      wrapperDiv.classList.add('image-advanced-align-center')
+      wrapperDiv.classList.add('image-advanced-align-left')
+    }
+
+    if (block.classList.contains('image-advanced-align-left')) {
+      block.classList.remove('image-advanced-align-left')
+      wrapperDiv.classList.add('image-advanced-align-left')
+    }
+
+    if (block.classList.contains('image-advanced-align-right')) {
+      block.classList.remove('image-advanced-align-right')
+      wrapperDiv.classList.add('image-advanced-align-right')
+    }
+
+    if (block.classList.contains('image-advanced-align-center')) {
       block.classList.remove('image-advanced-align-center')
+      wrapperDiv.classList.add('image-advanced-align-center')
     }
   }
 }
