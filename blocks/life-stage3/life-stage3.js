92b25be4e614f43a4186e754ff8322848589180b
Teodor Iancu <teodor.iancu@oegbverlag.at>
2024-07-27 23:22:01 +0200

    ls3

--- blocks/life-stage3/life-stage3.js
+++ blocks/life-stage3/life-stage3.js
@@ -1,34 +1,29 @@
 const badgeClass = 'badge-primary'
 
 export default function decorate(block) {
-  const [left, right] = block.children
+  const [leftDiv, rightDiv] = block.children
 
-  const topElement = document.createElement('div')
-  topElement.classList.add('top-element')
-  block.insertBefore(topElement, left)
+  const topDiv = document.createElement('div')
+  topDiv.classList.add('top-div')
+  block.insertBefore(topDiv, leftDiv, rightDiv)
 
-  const firstButtonContainer = left.querySelector('.button-container')
-  if (firstButtonContainer) {
-    topElement.appendChild(firstButtonContainer)
-  }
-
-  left?.classList.add('column-left')
-  right?.classList.add('column-right')
-
-  left?.querySelectorAll('ul').forEach((ul) => {
-    ul.classList.add('badges')
-    ul.querySelectorAll('li').forEach((li) => {
-      li.classList.add('badge', badgeClass)
-    })
-  })
+  leftDiv?.classList.add('column-left')
+  rightDiv?.classList.add('column-right')
 
   const columnsWrapper = document.createElement('div')
   columnsWrapper.classList.add('columns-wrapper')
-  columnsWrapper.appendChild(left)
-  columnsWrapper.appendChild(right)
+  columnsWrapper.appendChild(leftDiv)
+  columnsWrapper.appendChild(rightDiv)
   block.appendChild(columnsWrapper)
 
-  const secondButtonContainer = left.querySelectorAll('.button-container')[0]
+  ////////////////////////
+  // Move the first button to the top element
+  const firstButtonContainer = leftDiv.querySelector('.button-container')
+  if (firstButtonContainer) {
+    topDiv.appendChild(firstButtonContainer)
+  }
+
+  const secondButtonContainer = leftDiv.querySelectorAll('.button-container')[0]
   if (secondButtonContainer) {
     const secondButton = secondButtonContainer.querySelector('.button')
     if (secondButton) {
