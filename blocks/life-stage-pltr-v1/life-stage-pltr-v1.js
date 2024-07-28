763fc195618c479dcd2f6569215574c9bbc368f3
Teodor Iancu <teodor.iancu@oegbverlag.at>
2024-07-28 13:12:25 +0200

    Live Stage, Picture-Left Text-Right

--- blocks/life-stage-pltr-v1/life-stage-pltr-v1.js
+++ blocks/life-stage-pltr-v1/life-stage-pltr-v1.js
@@ -5,24 +5,26 @@ export default function decorate(block) {
 
   const topDiv = document.createElement('div')
   topDiv.classList.add('top-div')
-  block.insertBefore(topDiv, leftDiv, rightDiv)
+  block.insertBefore(topDiv, leftDiv)
 
   leftDiv?.classList.add('column-left')
   rightDiv?.classList.add('column-right')
 
   const columnsWrapper = document.createElement('div')
   columnsWrapper.classList.add('columns-wrapper')
-  columnsWrapper.appendChild(leftDiv)
+
+  // Reverse the order of columns
   columnsWrapper.appendChild(rightDiv)
+  columnsWrapper.appendChild(leftDiv)
   block.appendChild(columnsWrapper)
 
-  ////////////////////////
-  // Move the first button to the top element
+  // Move the first button to 'top-div'
   const firstButtonContainer = leftDiv.querySelector('.button-container')
   if (firstButtonContainer) {
     topDiv.appendChild(firstButtonContainer)
   }
 
+  // Replace first 'button'  with 'second-button'
   const secondButtonContainer = leftDiv.querySelectorAll('.button-container')[0]
   if (secondButtonContainer) {
     const secondButton = secondButtonContainer.querySelector('.button')
