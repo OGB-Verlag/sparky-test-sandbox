e75534deedf82b7c40f8bdb396b455fa7860af3f
Teodor Iancu <teodor.iancu@oegbverlag.at>
2024-07-28 15:15:01 +0200

    v2

---
+++ blocks/life-stage-pltr-v2/life-stage-pltr-v2.js
@@ -0,0 +1,42 @@
+export default function decorate(block) {
+  const [leftDiv, rightDiv] = block.children
+
+  const topDiv = document.createElement('div')
+  topDiv.classList.add('top-div')
+  block.insertBefore(topDiv, leftDiv)
+
+  leftDiv?.classList.add('column-left')
+  rightDiv?.classList.add('column-right')
+
+  const columnsWrapper = document.createElement('div')
+  columnsWrapper.classList.add('columns-wrapper')
+
+  // Reverse the order of columns
+  columnsWrapper.appendChild(rightDiv)
+  columnsWrapper.appendChild(leftDiv)
+  block.appendChild(columnsWrapper)
+
+  // Add badges to the Text column
+  rightDiv?.querySelectorAll('ul').forEach((ul) => {
+    ul.classList.add('badges')
+    ul.querySelectorAll('li').forEach((li) => {
+      li.classList.add('badge')
+    })
+  })
+
+  // Move the first button to 'top-div'
+  const firstButtonContainer = leftDiv.querySelector('.button-container')
+  if (firstButtonContainer) {
+    topDiv.appendChild(firstButtonContainer)
+  }
+
+  // Replace first 'button'  with 'second-button'
+  const secondButtonContainer = leftDiv.querySelectorAll('.button-container')[0]
+  if (secondButtonContainer) {
+    const secondButton = secondButtonContainer.querySelector('.button')
+    if (secondButton) {
+      secondButton.classList.remove('button')
+      secondButton.classList.add('second-button')
+    }
+  }
+}
