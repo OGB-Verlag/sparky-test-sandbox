5031faee199efbcfb82036b7b1de40da6f7ce036
Bernhard Waitz <bernhard.waitz@oegbverlag.at>
2024-07-31 12:50:59 +0200

    AOS integration

--- blocks/life-stage-pltr-v2/life-stage-pltr-v2.js
+++ blocks/life-stage-pltr-v2/life-stage-pltr-v2.js
@@ -1,46 +1,49 @@
-import { setupFadeInObserver } from '../../utils/animations/fadeIn.js'
+import { setupFadeInObserver } from '../../utils/animations/fadeIn.js';
 
 export default function decorate(block) {
-  const [leftDiv, rightDiv] = block.children
+  const [leftDiv, rightDiv] = block.children;
 
-  const topDiv = document.createElement('div')
-  topDiv.classList.add('top-div')
-  block.insertBefore(topDiv, leftDiv)
+  const topDiv = document.createElement('div');
+  topDiv.classList.add('top-div');
+  block.insertBefore(topDiv, leftDiv);
 
-  leftDiv?.classList.add('column-left')
-  rightDiv?.classList.add('column-right')
+  leftDiv?.classList.add('column-left');
+  rightDiv?.classList.add('column-right');
 
-  const columnsWrapper = document.createElement('div')
-  columnsWrapper.classList.add('columns-wrapper')
+  // AOS add Styling
+  leftDiv?.setAttribute('data-aos', 'fade-left');
+
+  const columnsWrapper = document.createElement('div');
+  columnsWrapper.classList.add('columns-wrapper');
 
   // Reverse the order of columns
-  columnsWrapper.appendChild(rightDiv)
-  columnsWrapper.appendChild(leftDiv)
-  block.appendChild(columnsWrapper)
+  columnsWrapper.appendChild(rightDiv);
+  columnsWrapper.appendChild(leftDiv);
+  block.appendChild(columnsWrapper);
 
   // Add badges to the Text column
   leftDiv?.querySelectorAll('ul').forEach((ul) => {
-    ul.classList.add('badges')
+    ul.classList.add('badges');
     ul.querySelectorAll('li').forEach((li) => {
-      li.classList.add('badge')
-    })
-  })
+      li.classList.add('badge');
+    });
+  });
 
   // Move the first button to 'top-div'
-  const firstButtonContainer = leftDiv.querySelector('.button-container')
+  const firstButtonContainer = leftDiv.querySelector('.button-container');
   if (firstButtonContainer) {
-    topDiv.appendChild(firstButtonContainer)
+    topDiv.appendChild(firstButtonContainer);
   }
 
   // Replace first 'button'  with 'second-button'
-  const secondButtonContainer = leftDiv.querySelectorAll('.button-container')[0]
+  const secondButtonContainer = leftDiv.querySelectorAll('.button-container')[0];
   if (secondButtonContainer) {
-    const secondButton = secondButtonContainer.querySelector('.button')
+    const secondButton = secondButtonContainer.querySelector('.button');
     if (secondButton) {
-      secondButton.classList.remove('button')
-      secondButton.classList.add('second-button')
+      secondButton.classList.remove('button');
+      secondButton.classList.add('second-button');
     }
   }
 
-  setupFadeInObserver([rightDiv], 2800, true) // `true` to allow multiple times observation
+  setupFadeInObserver([rightDiv], 2800, true); // `true` to allow multiple times observation
 }
