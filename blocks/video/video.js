4be3122d4b7ba8f33a0200dc61c5640316ed5c14
Teodor Iancu <teodor.iancu@oegbverlag.at>
2024-08-02 13:07:20 +0200

    revert

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,151 +1,140 @@
 /*
  * Video Block
- * Show a video referenced by a link
+ * Show videos and social posts directly on your page
  * https://www.hlx.live/developer/block-collection/video
  */
 
-const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)')
-
-function embedYoutube(url, autoplay, background) {
-  const usp = new URLSearchParams(url.search)
-  let suffix = ''
-  if (background || autoplay) {
-    const suffixParams = {
-      autoplay: autoplay ? '1' : '0',
-      mute: background ? '1' : '0',
-      controls: background ? '0' : '1',
-      disablekb: background ? '1' : '0',
-      loop: background ? '1' : '0',
-      playsinline: background ? '1' : '0',
-    }
-    suffix = `&${Object.entries(suffixParams)
-      .map(([k, v]) => `${k}=${encodeURIComponent(v)}`)
-      .join('&')}`
+const loadScript = (url, callback, type) => {
+  const head = document.querySelector('head')
+  const script = document.createElement('script')
+  script.src = url
+  if (type) {
+    script.setAttribute('type', type)
   }
+  script.onload = callback
+  head.append(script)
+  return script
+}
+
+const getDefaultVideo = (
+  url,
+) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+      <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen="" allow="autoplay"
+        scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
+      </iframe>
+    </div>`
+
+const videoYoutube = (url, autoplay) => {
+  const usp = new URLSearchParams(url.search)
+  const suffix = autoplay ? '&muted=1&autoplay=1' : ''
   let vid = usp.get('v') ? encodeURIComponent(usp.get('v')) : ''
-  const embed = url.pathname
+  const video = url.pathname
   if (url.origin.includes('youtu.be')) {
     ;[, vid] = url.pathname.split('/')
   }
 
-  const temp = document.createElement('div')
-  temp.innerHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-      <iframe src="https://www.youtube.com${
-        vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed
-      }" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
-      allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" allowfullscreen="" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
-    </div>`
-  return temp.children.item(0)
+  /*  const videoHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+        <iframe src="https://www.youtube.com${vid ? `/video/${vid}?rel=0&v=${vid}${suffix}` : video}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+        allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" allowfullscreen="" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
+        </div>`; 
+    */
+
+  const videoHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+        <iframe src="https://www.youtube.com/${video}?rel=0${suffix}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+        allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" allowfullscreen="" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
+      </div>`
+
+  return videoHTML
 }
 
-function embedVimeo(url, autoplay, background) {
+const videoVimeo = (url, autoplay) => {
   const [, video] = url.pathname.split('/')
-  let suffix = ''
-  if (background || autoplay) {
-    const suffixParams = {
-      autoplay: autoplay ? '1' : '0',
-      background: background ? '1' : '0',
-    }
-    suffix = `?${Object.entries(suffixParams)
-      .map(([k, v]) => `${k}=${encodeURIComponent(v)}`)
-      .join('&')}`
-  }
-  const temp = document.createElement('div')
-  temp.innerHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-      <iframe src="https://player.vimeo.com/video/${video}${suffix}" 
-      style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
-      frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen  
-      title="Content from Vimeo" loading="lazy"></iframe>
-    </div>`
-  return temp.children.item(0)
+  const suffix = autoplay ? '?muted=1&autoplay=1' : ''
+  const videoHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+        <iframe src="https://player.vimeo.com/video/${video}${suffix}" 
+        style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+        frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen  
+        title="Content from Vimeo" loading="lazy"></iframe>
+      </div>`
+  return videoHTML
 }
 
-function getVideoElement(source, autoplay, background) {
-  const video = document.createElement('video')
-  video.setAttribute('controls', '')
-  if (autoplay) video.setAttribute('autoplay', '')
-  if (background) {
-    video.setAttribute('loop', '')
-    video.setAttribute('playsinline', '')
-    video.removeAttribute('controls')
-    video.addEventListener('canplay', () => {
-      video.muted = true
-      if (autoplay) video.play()
-    })
-  }
-
-  const sourceEl = document.createElement('source')
-  sourceEl.setAttribute('src', source)
-  sourceEl.setAttribute('type', `video/${source.split('.').pop()}`)
-  video.append(sourceEl)
-
-  return video
+const videoTwitter = (url) => {
+  const videoHTML = `<blockquote class="twitter-tweet"><a href="${url.href}"></a></blockquote>`
+  loadScript('https://platform.twitter.com/widgets.js')
+  return videoHTML
 }
 
-const loadVideoEmbed = (block, link, autoplay, background) => {
-  if (block.dataset.embedLoaded === 'true') {
+// TODO: refine
+const videoMP4 = (url) => {
+  const videoHTML = `<video width="800" controls loop autoplay muted playsinline}>
+  <source src="${url.href}" type="video/mp4">
+  <source src="${url.href}" type="video/ogg">
+  Your browser does not support HTML video.
+</video>`
+  return videoHTML
+}
+
+const loadVideo = (block, link, autoplay) => {
+  if (block.classList.contains('video-is-loaded')) {
     return
   }
+
+  const VIDEO_CONFIG = [
+    {
+      match: ['youtube', 'youtu.be'],
+      video: videoYoutube,
+    },
+    {
+      match: ['vimeo'],
+      video: videoVimeo,
+    },
+    {
+      match: ['twitter'],
+      video: videoTwitter,
+    },
+    {
+      match: ['mp4'],
+      video: videoMP4,
+    },
+  ]
+
+  const config = VIDEO_CONFIG.find((e) => e.match.some((match) => link.includes(match)))
   const url = new URL(link)
-
-  const isYoutube = link.includes('youtube') || link.includes('youtu.be')
-  const isVimeo = link.includes('vimeo')
-
-  if (isYoutube) {
-    const embedWrapper = embedYoutube(url, autoplay, background)
-    block.append(embedWrapper)
-    embedWrapper.querySelector('iframe').addEventListener('load', () => {
-      block.dataset.embedLoaded = true
-    })
-  } else if (isVimeo) {
-    const embedWrapper = embedVimeo(url, autoplay, background)
-    block.append(embedWrapper)
-    embedWrapper.querySelector('iframe').addEventListener('load', () => {
-      block.dataset.embedLoaded = true
-    })
+  if (config) {
+    block.innerHTML = config.video(url, autoplay)
+    block.classList = `block video video-${config.match[0]}`
   } else {
-    const videoEl = getVideoElement(link, autoplay, background)
-    block.append(videoEl)
-    videoEl.addEventListener('canplay', () => {
-      block.dataset.embedLoaded = true
-    })
+    block.innerHTML = getDefaultVideo(url)
+    block.classList = 'block video'
   }
+  block.classList.add('video-is-loaded')
 }
 
-export default async function decorate(block) {
+export default function decorate(block) {
   const placeholder = block.querySelector('picture')
   const link = block.querySelector('a').href
   block.textContent = ''
-  block.dataset.embedLoaded = false
 
-  const autoplay = block.classList.contains('autoplay')
   if (placeholder) {
-    block.classList.add('placeholder')
     const wrapper = document.createElement('div')
     wrapper.className = 'video-placeholder'
-    wrapper.append(placeholder)
-
-    if (!autoplay) {
-      wrapper.insertAdjacentHTML(
-        'beforeend',
-        '<div class="video-placeholder-play"><button type="button" title="Play"></button></div>',
-      )
-      wrapper.addEventListener('click', () => {
-        wrapper.remove()
-        loadVideoEmbed(block, link, true, false)
-      })
-    }
+    wrapper.innerHTML = '<div class="video-placeholder-play"><button type="button" title="Play"></button></div>'
+    wrapper.prepend(placeholder)
+    wrapper.addEventListener('click', () => {
+      loadVideo(block, link, true)
+      block.querySelector('video').play()
+    })
     block.append(wrapper)
-  }
-
-  if (!placeholder || autoplay) {
+  } else {
     const observer = new IntersectionObserver((entries) => {
       if (entries.some((e) => e.isIntersecting)) {
         observer.disconnect()
-        const playOnLoad = autoplay && !prefersReducedMotion.matches
-        loadVideoEmbed(block, link, playOnLoad, autoplay)
+        loadVideo(block, link)
       }
     })
     observer.observe(block)
   }
+
+  // playVideoOnClick(block);
 }
