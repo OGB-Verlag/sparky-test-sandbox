849671a490e5da456928cd1c35118c08fa3f98c2
Teodor Iancu <teodor.iancu@oegbverlag.at>
2024-08-03 15:21:04 +0200

    backward-button

--- blocks/backward-button/backward-button.js
+++ blocks/backward-button/backward-button.js
@@ -1,16 +1,16 @@
-export default function decorate(block) {
-  const button = block.querySelector('.button')
+// export default function decorate(block) {
+//   const button = document.createElement('a')
+//   button.classList.add('backward-button')
+//   button.textContent = 'zurück zur Startseite'
+//   button.setAttribute('href', '/')
+//   button.style.textDecoration = 'underline'
 
-  if (button) {
-    // Insert icon before the button text
-    const backwardSVG = document.createElement('img')
-    backwardSVG.src = '../../icons/backward.svg'
-    backwardSVG.style.marginRight = '0.6rem'
-    button.insertBefore(backwardSVG, button.firstChild)
+//   const backwardSVG = document.createElement('img')
+//   backwardSVG.src = '../../icons/backward.svg'
+//   backwardSVG.style.marginRight = '0.5rem'
+//   backwardSVG.style.position = 'relative'
+//   backwardSVG.style.top = '4px'
+//   button.insertBefore(backwardSVG, button.firstChild)
 
-    // button.classList.remove('button')
-    button.classList.add('backward-button')
-    button.textContent = 'zurück zur Startseite'
-    button.setAttribute('href', '/')
-  }
-}
+//   block.appendChild(button)
+// }
