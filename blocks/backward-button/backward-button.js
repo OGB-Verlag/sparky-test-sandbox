4dbee3721f6c3f684d1ec618f7fcaac6ce314155
Teodor Iancu <teodor.iancu@oegbverlag.at>
2024-08-03 15:43:30 +0200

    backward-button test

--- blocks/backward-button/backward-button.js
+++ blocks/backward-button/backward-button.js
@@ -1,16 +1,16 @@
-// export default function decorate(block) {
-//   const button = document.createElement('a')
-//   button.classList.add('backward-button')
-//   button.textContent = 'zurück zur Startseite'
-//   button.setAttribute('href', '/')
-//   button.style.textDecoration = 'underline'
+export default function decorate(block) {
+  const button = document.createElement('a')
+  button.classList.add('backward-button')
+  button.textContent = 'zurück zur Startseite'
+  button.setAttribute('href', '/')
+  button.style.textDecoration = 'underline'
 
-//   const backwardSVG = document.createElement('img')
-//   backwardSVG.src = '../../icons/backward.svg'
-//   backwardSVG.style.marginRight = '0.5rem'
-//   backwardSVG.style.position = 'relative'
-//   backwardSVG.style.top = '4px'
-//   button.insertBefore(backwardSVG, button.firstChild)
+  const backwardSVG = document.createElement('img')
+  backwardSVG.src = '../../icons/backward.svg'
+  backwardSVG.style.marginRight = '0.5rem'
+  backwardSVG.style.position = 'relative'
+  backwardSVG.style.top = '4px'
+  button.insertBefore(backwardSVG, button.firstChild)
 
-//   block.appendChild(button)
-// }
+  block.appendChild(button)
+}
