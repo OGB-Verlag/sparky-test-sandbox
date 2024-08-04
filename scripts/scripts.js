49c34477b74955a5ecfd27876bf9c7cf578c670c
Teodor Iancu <teodor.iancu@oegbverlag.at>
2024-08-04 15:53:08 +0200

    subpages

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -386,7 +386,6 @@ async function loadingStylesAndScripts() {
   // load custom css files
   const customCss = [
     `${window.hlx.codeBasePath}/styles/reset.css`,
-    `${window.hlx.codeBasePath}/styles/finanzierung/finanzierung.css`,
     `${window.hlx.codeBasePath}/styles/base-style/base-style.css`,
     `${window.hlx.codeBasePath}/styles/subpages/subpages.css`,
     `${window.hlx.codeBasePath}/styles/custom/mobile-sticky-button.css`,
